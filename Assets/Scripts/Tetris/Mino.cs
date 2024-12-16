87d91f23977d3bf8cb88dd5083f1dbeb36ea05af
bluetangerine0420 <a01051558311@gmail.com>
2024-12-16 20:16:30 +0900

    tetris

--- Assets/Scripts/Tetris/Mino.cs
+++ Assets/Scripts/Tetris/Mino.cs
@@ -20,20 +20,23 @@ public class Mino : MonoBehaviour
 
     [SerializeField] private float m_moveValue;
 
+    [SerializeField] private float m_softDropTime;
+    [SerializeField] private bool m_softDrop;
+
     [SerializeField] private bool m_isOn;
     [SerializeField] private MinoKind m_kind;
 
     private void Start()
     {
-        //StartCoroutine(drop());
+        StartCoroutine(drop());
+        StartCoroutine(PlayerDrop());
     }
 
     private void Update()
     {
-        if(Input.GetKey(KeyCode.S)) transform.position += new Vector3(0, -m_dropValue, 0);
-        if(Input.GetKeyUp(KeyCode.D)) transform.position += new Vector3(m_moveValue, 0, 0);
-        if(Input.GetKeyUp(KeyCode.A)) transform.position += new Vector3(-m_moveValue, 0, 0);
-        
+        //if (Input.GetKey(KeyCode.S)) transform.position += new Vector3(0, -m_dropValue, 0);
+        if (Input.GetKey(KeyCode.D)) transform.position += new Vector3(m_moveValue, 0, 0);
+        if (Input.GetKey(KeyCode.A)) transform.position += new Vector3(-m_moveValue, 0, 0);
     }
 
     public void SetOn(bool pOn)
@@ -43,13 +46,52 @@ public class Mino : MonoBehaviour
 
     private IEnumerator drop()
     {
-        while(true)
+        while (m_isOn)
         {
             yield return new WaitForSeconds(m_dropSpeed);
+
+            if(!m_softDrop)
             transform.position += new Vector3(0, -m_dropValue, 0);
+        }
 
-        }        
+        yield break;
+    }
 
+    private IEnumerator PlayerDrop()
+    {
+        float curTime = 0;
+
+        while (m_isOn)
+        {
+            if(Input.GetKeyDown(KeyCode.S))
+                transform.position += new Vector3(0, -m_dropValue, 0);
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                if (curTime >= m_softDropTime)
+                {
+                    m_softDrop = true;
+                    {
+                        transform.position += new Vector3(0, -m_dropValue, 0);
+                        curTime = 0;
+                        Debug.Log("down");
+                    }
+                }
+
+                else
+                {
+                    curTime += Time.deltaTime;
+                    Debug.Log("charging");
+                }
+                yield return new WaitForEndOfFrame();
+            }
+            else
+            {
+                curTime = 0;
+                m_softDrop = false;
+            }
+            yield return new WaitForEndOfFrame();
+        }
         yield break;
     }
 }
