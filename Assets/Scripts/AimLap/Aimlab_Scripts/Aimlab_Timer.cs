fc6497da71cec607f31c83a1bab2ab9e6a7a13c7
Potato1016 <esky1004@gmail.com>
2024-12-16 23:26:24 +0900

    Aimlab

---
+++ Assets/Scripts/AimLap/Aimlab_Scripts/Aimlab_Timer.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using TMPro;
+
+public class Aimlab_Timer : MonoBehaviour
+{
+    public TMP_Text timerText; 
+    private float timeLeft = 60f; 
+    private bool timerRunning = true;
+
+    void Start()
+    {
+   
+        UpdateTimerText();  
+    }
+
+    void Update()
+    {
+        if (timerRunning)
+        {
+            timeLeft -= Time.deltaTime; 
+
+        
+            if (timeLeft <= 0)
+            {
+                timeLeft = 0;
+                timerRunning = false;
+                OnTimerEnd(); 
+            }
+
+            UpdateTimerText();
+        }
+    }
+
+    void UpdateTimerText()
+    {
+       
+        timerText.text = Mathf.CeilToInt(timeLeft).ToString();
+    }
+
+    void OnTimerEnd()
+    {
+        Debug.Log("타이머 종료!");
+            
+    }
+}
