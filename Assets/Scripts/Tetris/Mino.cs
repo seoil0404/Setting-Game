e79c5b4ee21694abcd1c8e642acc3d8011bdaa53
bluetangerine0420 <a01051558311@gmail.com>
2024-12-17 09:07:02 +0900

    Tetris

--- Assets/Scripts/Tetris/Mino.cs
+++ Assets/Scripts/Tetris/Mino.cs
@@ -15,16 +15,19 @@ public enum MinoKind
 
 public class Mino : MonoBehaviour
 {
-    [SerializeField] private float m_dropValue;
-    [SerializeField] private float m_dropSpeed;
+    public float m_dropSpeed;
+    public int m_dropValue;
 
-    [SerializeField] private float m_moveValue;
+    public float m_moveValue;
 
     [SerializeField] private float m_softDropTime;
-    [SerializeField] private bool m_softDrop;
+    private bool m_softDrop;
 
     [SerializeField] private bool m_isOn;
     [SerializeField] private MinoKind m_kind;
+
+    [SerializeField] private LayerMask m_bottomLayer;
+    [SerializeField] private LayerMask m_blockLayer;
 
     private void Start()
     {
@@ -35,6 +38,12 @@ public class Mino : MonoBehaviour
     private void Update()
     {
         //if (Input.GetKey(KeyCode.S)) transform.position += new Vector3(0, -m_dropValue, 0);
+        Move();
+
+    }
+
+    public void Move()
+    {
         if (Input.GetKey(KeyCode.D)) transform.position += new Vector3(m_moveValue, 0, 0);
         if (Input.GetKey(KeyCode.A)) transform.position += new Vector3(-m_moveValue, 0, 0);
     }
@@ -44,14 +53,18 @@ public class Mino : MonoBehaviour
         m_isOn = pOn;
     }
 
+    private void Down()
+    {
+        transform.position += new Vector3(0, -0.5f, 0);
+    }
+
     private IEnumerator drop()
     {
         while (m_isOn)
         {
-            yield return new WaitForSeconds(m_dropSpeed);
+            if (!m_softDrop) Down();
 
-            if(!m_softDrop)
-            transform.position += new Vector3(0, -m_dropValue, 0);
+            yield return new WaitForSeconds(m_dropSpeed);
         }
 
         yield break;
@@ -63,8 +76,10 @@ public class Mino : MonoBehaviour
 
         while (m_isOn)
         {
-            if(Input.GetKeyDown(KeyCode.S))
-                transform.position += new Vector3(0, -m_dropValue, 0);
+            Debug.Log("player");
+
+            if (Input.GetKeyDown(KeyCode.S))
+                Down();
 
             if (Input.GetKey(KeyCode.S))
             {
@@ -72,8 +87,7 @@ public class Mino : MonoBehaviour
                 {
                     m_softDrop = true;
                     {
-                        transform.position += new Vector3(0, -m_dropValue, 0);
-                        curTime = 0;
+                        Down(); curTime = 0;
                         Debug.Log("down");
                     }
                 }
@@ -94,5 +108,18 @@ public class Mino : MonoBehaviour
         }
         yield break;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("뭐 닿임");
+        Debug.Log(collision.gameObject.layer);
+        Debug.Log(m_bottomLayer);
+        // 충돌한 객체의 레이어가 groundLayer에 포함되어 있는지 확인
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Bottom") || collision.gameObject.layer == LayerMask.NameToLayer("Block"))
+        {
+            m_isOn = false;
+        }
+    }
+
 }
 
