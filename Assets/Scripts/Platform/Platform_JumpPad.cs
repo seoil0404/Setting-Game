82f34bb5083bcbbb754b9a2e77d30f994e94e0f5
seoil0404 <seoil6795@gmail.com>
2024-12-17 12:49:14 +0900

    Map Updated

--- Assets/Scripts/Platform/Platform_JumpPad.cs
+++ Assets/Scripts/Platform/Platform_JumpPad.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class Platform_JumpPad : MonoBehaviour
 {
+    [SerializeField] private float jumpTime;
+    [SerializeField] private Vector3 jumpDirection;
+
+    readonly float baseJumpPowerMultiplier = 2f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            collision.gameObject.GetComponent<Platform_Player>().OnJumpPad();
+            collision.gameObject.GetComponent<Platform_Player>().OnJumpPad(jumpTime, jumpDirection * baseJumpPowerMultiplier);
         }
     }
 }
