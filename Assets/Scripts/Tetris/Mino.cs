d4063eda063a1d5e5800e18e12c0c68602cddae7
bluetangerine0420 <a01051558311@gmail.com>
2024-12-17 19:30:11 +0900

    Tetris

--- Assets/Scripts/Tetris/Mino.cs
+++ Assets/Scripts/Tetris/Mino.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework.Constraints;
 using System.Collections;
+using System.Threading;
 using UnityEngine;
 
 public enum MinoKind
@@ -13,6 +14,11 @@ public enum MinoKind
     Z
 }
 
+public enum Direction
+{
+    Up, Down, Left, Right
+}
+
 public class Mino : MonoBehaviour
 {
     public float m_dropSpeed;
@@ -23,29 +29,67 @@ public class Mino : MonoBehaviour
     [SerializeField] private float m_softDropTime;
     private bool m_softDrop;
 
-    [SerializeField] private bool m_isOn;
+    public bool m_isOn;
     [SerializeField] private MinoKind m_kind;
+    private Direction m_direction;
 
-    [SerializeField] private LayerMask m_bottomLayer;
-    [SerializeField] private LayerMask m_blockLayer;
+    [SerializeField] private string m_bottomLayer;
+    [SerializeField] private string m_blockLayer;
 
+    [SerializeField] private GameObject m_leftCollider;
+    [SerializeField] private GameObject m_rightCollider;
+    [SerializeField] private GameObject m_upCollider;
+    [SerializeField] private GameObject m_downCollider;
     private void Start()
     {
         StartCoroutine(drop());
         StartCoroutine(PlayerDrop());
+        m_direction = Direction.Up;
     }
 
     private void Update()
     {
-        //if (Input.GetKey(KeyCode.S)) transform.position += new Vector3(0, -m_dropValue, 0);
+        if (Input.GetKey(KeyCode.S)) transform.position += new Vector3(0, -m_dropValue, 0);
         Move();
-
+        Spin();
     }
 
     public void Move()
     {
-        if (Input.GetKey(KeyCode.D)) transform.position += new Vector3(m_moveValue, 0, 0);
-        if (Input.GetKey(KeyCode.A)) transform.position += new Vector3(-m_moveValue, 0, 0);
+        if (m_isOn)
+        {
+            if (Input.GetKeyDown(KeyCode.D)) transform.position += new Vector3(m_moveValue, 0, 0);
+            if (Input.GetKeyDown(KeyCode.A)) transform.position += new Vector3(-m_moveValue, 0, 0);
+        }
+    }
+
+    public void Spin()
+    {
+        if (m_isOn)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                transform.Rotate(0, 0, -90f);
+                switch (m_direction)
+                {
+                    case Direction.Up: m_direction = Direction.Left; break;
+                    case Direction.Down: m_direction = Direction.Right; break;
+                    case Direction.Left: m_direction = Direction.Down; break;
+                    case Direction.Right: m_direction = Direction.Up; break;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                transform.Rotate(0, 0, 90f);
+                switch (m_direction)
+                {
+                    case Direction.Up: m_direction = Direction.Right; break;
+                    case Direction.Down: m_direction = Direction.Left; break;
+                    case Direction.Left: m_direction = Direction.Up; break;
+                    case Direction.Right: m_direction = Direction.Down; break;
+                }
+            }
+        }
     }
 
     public void SetOn(bool pOn)
@@ -115,11 +159,67 @@ public class Mino : MonoBehaviour
         Debug.Log(collision.gameObject.layer);
         Debug.Log(m_bottomLayer);
         // 충돌한 객체의 레이어가 groundLayer에 포함되어 있는지 확인
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Bottom") || collision.gameObject.layer == LayerMask.NameToLayer("Block"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer(m_bottomLayer))
         {
             m_isOn = false;
+            ColliderSet(m_direction);
+        }
+        else if (collision.gameObject.layer == LayerMask.NameToLayer(m_blockLayer))
+        {
+            m_isOn = false;
+            //Down();
         }
     }
 
+    private void ColliderSet(Direction pDierction)
+    {
+        m_downCollider.SetActive(false);
+        m_upCollider.SetActive(false);
+        m_leftCollider.SetActive(false);
+        m_rightCollider.SetActive(false);
+
+        switch (pDierction)
+        {
+            case Direction.Right:
+                {
+                    m_rightCollider.SetActive(true);
+                    ChangeLayer(m_rightCollider, m_bottomLayer);
+                    break;
+                }
+            case Direction.Left:
+                {
+                    m_leftCollider.SetActive(true);
+                    ChangeLayer(m_leftCollider, m_bottomLayer);
+                    break;
+                }
+            case Direction.Up:
+                {
+                    m_upCollider.SetActive(true);
+                    ChangeLayer(m_upCollider, m_bottomLayer);
+                    break;
+                }
+            case Direction.Down:
+                {
+                    m_downCollider.SetActive(true);
+                    ChangeLayer(m_downCollider,m_bottomLayer);
+                    break;
+                }
+        }
+    }
+
+    void ChangeLayer(GameObject pObject, string pLayerName)
+    {
+        int layerIndex = LayerMask.NameToLayer(pLayerName);
+
+        if (layerIndex == -1)
+        {
+            Debug.LogError("Layer name '" + pLayerName + "' does not exist.");
+            return;
+        }
+
+        // 레이어 변경
+        pObject.layer = layerIndex;
+        Debug.Log($"Layer changed to: {pLayerName}");
+    }
 }
 
--- Assets/Scripts/Tetris/TetrisGrid.cs
+++ Assets/Scripts/Tetris/TetrisGrid.cs
@@ -2,18 +2,40 @@ using UnityEngine;
 
 public class TetrisGrid : MonoBehaviour
 {
+    public static TetrisGrid Instance;
+
     public int width = 10;
     public int height = 20;
 
-    private bool[,] grid;
+    private Transform[,] grid;
 
-    [SerializeField] private Vector2[] m_widthline;
-    [SerializeField] private Vector2[] m_heightline;
+    [SerializeField] private Vector2[] m_widthLineVec;
+    [SerializeField] private Vector2[] m_heightLineVec;
 
-    public GameObject[] m_gameObject;
+    [SerializeField] private GameObject m_widthLine;
+    [SerializeField] private GameObject m_heightLine;
+
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        grid = new Transform[width, height]; // 2D 배열 초기화
+    }
+
     void Start()
     {
-        grid = new bool[width, height];
+        //grid = new bool[width, height]; 
+        for (int i = 0; i < m_heightLineVec.Length; i++)
+        {
+            Instantiate(m_heightLine, new Vector3(0, m_heightLineVec[i].y), Quaternion.identity);
+            //Debug.DrawLine(new Vector3(m_widthLineVec[m_widthLineVec.Length - 1].x, m_heightLineVec[i].y), new Vector3(m_widthLineVec[0].x, m_heightLineVec[i].y), Color.black);
+        }
+        for (int i = 0; i < m_widthLineVec.Length; i++)
+        {
+            Instantiate(m_widthLine, new Vector3(m_widthLineVec[i].x, 0), m_widthLine.transform.rotation);
+
+            //Debug.DrawLine(new Vector3(m_widthLineVec[i].x, m_heightLineVec[m_heightLineVec.Length - 1].y), new Vector3(m_widthLineVec[i].x, m_heightLineVec[0].y), Color.black);
+        }
     }
 
 
@@ -24,13 +46,35 @@ public class TetrisGrid : MonoBehaviour
         //{
         //    m_gameObject[i].transform.position = new Vector3(m_widthline[m_widthline.Length - 1].x, m_heightline[i].y),new Vector3(m_widthline[0].x, m_heightline[i].y);
         //}
-        for (int i = 0; i < m_heightline.Length; i++)
-            Debug.DrawLine(new Vector3(m_widthline[m_widthline.Length - 1].x, m_heightline[i].y), new Vector3(m_widthline[0].x, m_heightline[i].y),Color.black);
-        for (int i = 0; i < m_widthline.Length; i++)
-            Debug.DrawLine(new Vector3(m_widthline[i].x, m_heightline[m_heightline.Length - 1].y), new Vector3(m_widthline[i].x, m_heightline[0].y),Color.black);
+        for (int i = 0; i < m_heightLineVec.Length; i++)
+        {
+            //Instantiate(m_heightLine, new Vector3(0, m_heightLineVec[i].y), Quaternion.identity);
+            Debug.DrawLine(new Vector3(m_widthLineVec[m_widthLineVec.Length - 1].x, m_heightLineVec[i].y), new Vector3(m_widthLineVec[0].x, m_heightLineVec[i].y),Color.black);
+        }
+        for (int i = 0; i < m_widthLineVec.Length; i++)
+        {
+           //    Instantiate(m_widthLine, new Vector3(m_widthLineVec[i].x, 0), Quaternion.identity);
+
+            Debug.DrawLine(new Vector3(m_widthLineVec[i].x, m_heightLineVec[m_heightLineVec.Length - 1].y), new Vector3(m_widthLineVec[i].x, m_heightLineVec[0].y),Color.black);
+        }
 
     }
 
+    private Vector2 RoundToGrid(Vector2 pos)
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+    public void SaveBlockToGrid(Transform block)
+    {
+        foreach (Transform tile in block)
+        {
+            Vector2 pos = RoundToGrid(tile.position);
+            int x = (int)pos.x;
+            int y = (int)pos.y;
+
+            grid[x, y] = tile; // 블록 조각을 그리드에 기록
+        }
+    }
     void AddBlock(int pX, int pY)
     {
 
